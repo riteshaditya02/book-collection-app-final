fe325361f3445aad95ad554c8681e576590d3b99
G RITESH ADITYA <102183232+riteshaditya02@users.noreply.github.com>
2024-02-27 16:18:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: ["https://book-collection-app-eight.vercel.app/"],
+    origin: ["https://book-collection-app-final.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
 }));
